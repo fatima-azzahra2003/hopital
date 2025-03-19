a2986ff6afa6e121e848c69c92c69f0bb1fb951f
fatima-azzahra2003 <fatimaazzahrahamdaoui3@gmail.com>
2025-03-19 12:34:11 +0000

    partie 2

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.hopital.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repositories.PatientRepository;
@@ -7,8 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -17,18 +18,35 @@ public class PatientController {
 
     @GetMapping("/patients")
     public String index(Model model,
-                        @RequestParam(name="page", defaultValue="0") int page,
-                        @RequestParam(name="size", defaultValue="4") int size,
-                        @RequestParam(name="keyword", defaultValue="") String keyword) {
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "4") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
         Page<Patient> pagePatients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
         model.addAttribute("patients", pagePatients.getContent());
         model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 
-    @GetMapping(path = "/deletePatient")
-    public String delete(@RequestParam Long id) {
+    @GetMapping("/deletePatient")
+    public String delete(@RequestParam Long id, @RequestParam(name = "keyword", defaultValue = "") String keyword,
+                         @RequestParam(name = "page", defaultValue = "0") int page) {
         patientRepository.deleteById(id);
+        return "redirect:/patients?page=" + page + "&keyword=" + keyword;
+    }
+
+    @GetMapping("/formPatient")
+    public String formPatient(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatient";
+    }
+
+    @PostMapping("/savePatient")
+    public String savePatient(@Valid Patient patient, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "formPatient";
+        }
+        patientRepository.save(patient);
         return "redirect:/patients";
     }
 }
